bd62ec03fb2294c44b95ce922052d92958bc9998
Thanh Dat <datgd1012@gmail.com>
2026-03-31 22:19:19 +0700

    feat: update OPT algorithm and main test

--- CODE/main.py
+++ CODE/main.py
@@ -1,6 +1,6 @@
 from algorithms.fifo import FIFO
 from algorithms.lru import LRU
-
+from algorithms.opt import OPT
 if __name__ == "__main__":
     ref_list = [7, 0, 1, 2, 0, 3, 0, 4]
     frame_count = 3
@@ -28,3 +28,16 @@ if __name__ == "__main__":
 
     lru_faults = sum(1 for r in lru_result if r["fault"])
     print("Total LRU faults:", lru_faults)
+    
+    print("\n")
+    
+    #OPT
+    opt = OPT()
+    opt_result = opt.simulate(ref_list, frame_count)
+
+    print("=== OPT ===")
+    for r in opt_result:
+        print(r)
+
+    opt_faults = sum(1 for r in opt_result if r["fault"])
+    print("Total OPT faults:", opt_faults)
